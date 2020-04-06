58e229f14359701f5ea0791fd5d42c4ffb8917c0
amirali175 <amirdek511@gmail.com>
2020-04-06 18:58:25 -0400

    :horse:

--- b000000000.py
+++ b000000000.py
@@ -19,9 +19,23 @@ eleves["Corlings"]=[]
 eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
+
+def personne_elue(name):
+    return name == 'Zoureni'
+
 def search(name):
-   print( len(eleves.values()) )
-   return False
+   from collections import deque
+   search_queue = deque()
+   search_queue += eleves[name]
+
+   while search_queue:
+      personne = search_queue.popleft()
+      if personne_elue(name):
+         print(name + " a le fameux Mac")
+         return True
+      search_queue += eleves[name]
+      return False
+
 
 if __name__== "__main__":
   search("Boris")
